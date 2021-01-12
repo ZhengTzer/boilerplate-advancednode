ce12d053b7af1313f6c74e1d350d28ece078d038
ZhengTzer <zhengtzer@gmail.com>
2021-01-12 17:55:31 +0800

    add How to Use Passport Strategies

--- server.js
+++ server.js
@@ -10,24 +10,6 @@ const session = require('express-session')
 const ObjectID = require('mongodb').ObjectID
 const LocalStrategy = require('passport-local')
 
-passport.use(
-  new LocalStrategy(function (username, password, done) {
-    myDataBase.findOne({ username: username }, function (err, user) {
-      console.log('User ' + username + ' attempted to log in.')
-      if (err) {
-        return done(err)
-      }
-      if (!user) {
-        return done(null, false)
-      }
-      if (password !== user.password) {
-        return done(null, false)
-      }
-      return done(null, user)
-    })
-  })
-)
-
 // start
 const app = express()
 app.set('view engine', 'pug')
@@ -57,9 +39,25 @@ myDB(async (client) => {
   app.route('/').get((req, res) => {
     //Change the response to render the Pug template
     res.render('pug', {
+      showLogin: true,
       title: 'Connected to Database',
       message: 'Please login'
     })
+  })
+
+  // login
+  app
+    .route('/login')
+    .post(
+      passport.authenticate('local', { failureRedirect: '/' }),
+      (req, res) => {
+        res.redirect('/profile')
+      }
+    )
+
+  // profile
+  app.route('/profile').get((req, res) => {
+    res.render(process.cwd() + '/views/pug/profile')
   })
 
   // Serialization and deserialization here...
@@ -73,6 +71,24 @@ myDB(async (client) => {
     })
   })
 
+  passport.use(
+    new LocalStrategy(function (username, password, done) {
+      myDataBase.findOne({ username: username }, function (err, user) {
+        console.log('User ' + username + ' attempted to log in.')
+        if (err) {
+          return done(err)
+        }
+        if (!user) {
+          return done(null, false)
+        }
+        if (password !== user.password) {
+          return done(null, false)
+        }
+        return done(null, user)
+      })
+    })
+  )
+
   // Be sure to add this...
 }).catch((e) => {
   app.route('/').get((req, res) => {
@@ -80,6 +96,7 @@ myDB(async (client) => {
   })
 })
 
+// port listening
 app.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port ' + process.env.PORT)
 })
