255656474ee078a63c18acbe35a56d12f54183d8
ZhengTzer <zhengtzer@gmail.com>
2021-01-12 17:42:05 +0800

    add Authentication Strategies

--- server.js
+++ server.js
@@ -3,9 +3,30 @@ require('dotenv').config()
 const express = require('express')
 const myDB = require('./connection')
 const fccTesting = require('./freeCodeCamp/fcctesting.js')
+
+// passport
 const passport = require('passport')
 const session = require('express-session')
 const ObjectID = require('mongodb').ObjectID
+const LocalStrategy = require('passport-local')
+
+passport.use(
+  new LocalStrategy(function (username, password, done) {
+    myDataBase.findOne({ username: username }, function (err, user) {
+      console.log('User ' + username + ' attempted to log in.')
+      if (err) {
+        return done(err)
+      }
+      if (!user) {
+        return done(null, false)
+      }
+      if (password !== user.password) {
+        return done(null, false)
+      }
+      return done(null, user)
+    })
+  })
+)
 
 // start
 const app = express()
