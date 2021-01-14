0cdaf25c8bd71eb7ba8eb6983e00b1a6119a1113
ZhengTzer <zhengtzer@gmail.com>
2021-01-14 15:28:27 +0800

    minor fix

--- auth.js
+++ auth.js
@@ -2,6 +2,7 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const bcrypt = require('bcrypt')
 const ObjectID = require('mongodb').ObjectID
+const GitHubStrategy = require('passport-github').Strategy
 
 module.exports = function (app, myDataBase) {
   passport.serializeUser((user, done) => {
